73072ec461eb550febfa5c46e12574f92ac30e68
Jürgen Geuter <tante@the-gay-bar.com>
2008-08-04 18:30:09 +0200

    Added my screenlayout scripts for fast switching of display configurations on the notebook.

---
+++ .screenlayout/default.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output LVDS --mode 1280x800 --pos 0x0 --rotate normal --output TMDS-1 --off --output VGA --off
---
+++ .screenlayout/with_tv.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output LVDS --mode 1280x800 --pos 0x0 --rotate normal --output TMDS-1 --off --output VGA --mode 1360x768 --pos 1416x0 --rotate normal
