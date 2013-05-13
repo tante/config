a6dcba07f8b0d7fd3056b2f463bc5be878d5c6f8
Jürgen 'tante' Geuter <tante@tante.cc>
2013-05-13 15:06:23 +0200

    Added theme thingy

--- setup.zsh
+++ setup.zsh
@@ -2,6 +2,9 @@ CONFIG=$HOME/config
 
 FILES=".vimrc .vim .zshrc .zshenv .pystartup .gitconfig .gvimrc .gitignore"
     
+# Special Treatment for oh-my-zsh-theme
+ln -f -s $CONFIG/customization/themes $CONFIG/oh-my-zsh/custom/
+
 echo "Setting up Links"
 for i in $FILES; do
     echo "Linking $i"
